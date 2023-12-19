d58f0609c99d3cc7af5e96026d404ff10a9fa0ee
OK-Ro <o.robert1994@hotmail.com>
2023-12-19 20:35:27 +0100

    fronted

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = {
   question: string;
@@ -9,6 +10,57 @@ type Props = {
   totalQuestions: number;
 };
 
+const QuizContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f4f4f4;
+`;
+
+const QuestionCardContainer = styled.div`
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Number = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const QuestionText = styled.p`
+  font-size: 20px;
+  margin-bottom: 15px;
+`;
+
+const AnswerButton = styled.button`
+  margin-bottom: 10px;
+  padding: 10px;
+  width: 100%;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #45a049;
+  }
+
+  &:disabled {
+    background-color: #ddd;
+    cursor: not-allowed;
+  }
+`;
+
 function QuestionCard({
   question,
   answers,
@@ -18,23 +70,28 @@ function QuestionCard({
   totalQuestions,
 }: Props) {
   return (
-    <div>
-      <p className="number">
-        Question: {questionNr} / {totalQuestions}
-      </p>
+    <QuizContainer>
+      <QuestionCardContainer>
+        <Number>
+          Question: {questionNr} / {totalQuestions}
+        </Number>
 
-      <p dangerouslySetInnerHTML={{ __html: question }} />
+        <QuestionText dangerouslySetInnerHTML={{ __html: question }} />
 
-      <div>
-        {answers.map((answer) => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
-              <span dangerouslySetInnerHTML={{ __html: answer }} />
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
+        <div>
+          {answers.map((answer, index) => (
+            <div key={index}>
+              <AnswerButton
+                disabled={userAnswer}
+                onClick={() => callback(answer)}
+              >
+                <span dangerouslySetInnerHTML={{ __html: answer }} />
+              </AnswerButton>
+            </div>
+          ))}
+        </div>
+      </QuestionCardContainer>
+    </QuizContainer>
   );
 }
 
